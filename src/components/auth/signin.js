649d20aea1779d6e615194a53a86f61a383f2f1d
PC\Adonis <adonis0444@gmail.com>
2019-02-18 23:59:08 -0500

    creating the signup and signin component with a header wrapper

---
+++ src/components/auth/signin.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Signin extends Component {
+    render() {
+        return(
+            <div className='sign-in'>
+                Signin
+            </div>
+        )
+    }
+}
+
+export default Signin;
