b8c30d5ab2b1abb1801bf91c53ec6ca5f200d04c
PC\Adonis <adonis0444@gmail.com>
2019-02-23 17:46:52 -0500

    Creating sign up form

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import SignupForm from './signupForm';
 
 class Signup extends Component {
     render() {
         return(
             <div className='sign-up'>
-                Signup
+                <SignupForm/>
             </div>
         )
     }
---
+++ src/components/auth/signupForm.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
+
+import FormTitle from '../formTitle';
+import { FormInput, FormButton } from '../formFields';
+import TextLink from '../textLink';
+
+class SignupForm extends Component {
+    render() {
+        return(
+            <form className='sign-up-form'>
+                <FormTitle className='sign-up-form__title' text='New User'/>
+                {/* Changes */}
+                <Field className='sign-up-form__fullname' component={FormInput} name='fullname' type='text' placeholder='Enter Your Full Name' title='Full Name'/>
+                <Field className='sign-up-form__unit' component={FormInput} name='unit' type='text' placeholder='Enter Unit #' title='Unit #'/>
+                {/* End Changes */}
+                <Field className='sign-up-form__email' component={FormInput} name='email' type='email' placeholder='Email' title='Email'/>
+                <Field className='sign-up-form__password' component={FormInput} name='password' type='password' placeholder='Password' title='Password'/>
+                <Field className='sign-up-form__create-account' component={FormButton} name='createAccount' type='submit' title='Create Account'/>
+
+                <div className='sign-up-form__text-links'>
+                    <TextLink to='/signin' text='Already Registered? Sign in'/>
+                </div>
+            </form>
+        )
+    }
+}
+
+SignupForm = reduxForm({
+    form: 'signup'
+})(SignupForm);
+
+export default SignupForm;
