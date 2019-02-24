5253cb4fcc9bc9ebbf1517f3fedb2c84c89f0d02
PC\Adonis <adonis0444@gmail.com>
2019-02-23 21:35:25 -0500

    store authenticated user in the state

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,12 +1,18 @@
 import axios from 'axios';
 import { ROOT_URL } from '../config';
+import {
+    AUTHENTICATE_USER
+} from './types';
 
 export function signUp(fields, success){
     console.log(fields);
     return function(dispatch){
         axios.post(`${ROOT_URL}/signUp`, fields)
-        .then(response => {
-                console.log(response);
+            .then(response => {
+                dispatch({
+                    type: AUTHENTICATE_USER,
+                    payload: response.data
+                });
                 success();
             })
             .catch(err => {
