71a4b8903e718d342fcf6b27bd2df86eaa01f084
PC\Adonis <adonis0444@gmail.com>
2019-02-23 21:08:37 -0500

    Saving the post request

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -4,13 +4,14 @@ import { ROOT_URL } from '../config';
 export function signUp(fields, success){
     console.log(fields);
     return function(dispatch){
-        // axios.post(`${ROOT_URL}/signUp`, fields)
-        //     .then(response => {
-
-        //     })
-        //     .catch(err => {
-        //         if(err)
-        //             console.log(err);
-        //     });
+        axios.post(`${ROOT_URL}/signUp`, fields)
+        .then(response => {
+                console.log(response);
+                success();
+            })
+            .catch(err => {
+                if(err)
+                    console.log(err);
+            });
     }
 }
