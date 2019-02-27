36d50c5eea44bb61b743de6959d8f7875b29f001
PC\Adonis <adonis0444@gmail.com>
2019-02-26 22:35:59 -0500

    Creating achive component

---
+++ src/components/newsletter/newsletterArchive.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+export function ArchiveItem({title, date}){
+    return (
+        <div className='archive-item'>
+            <div className='archive-item__title'>{ title }</div>
+            <div className='archive-item__date'>{ date }</div>
+        </div>
+    )
+}
+
+class NewsletterArchive extends Component {
+    render() {
+        return (
+            <div className='newsletter-archive'>
+                <div className='newsletter-archive__title'>Archive</div>
+                <div className='newsletter-archive__items archive-items'>
+                    <ArchiveItem title='Hey' date='this is the date'/>
+                    <ArchiveItem title='Hey' date='this is the date'/>
+                    <ArchiveItem title='Hey' date='this is the date'/>
+                    <ArchiveItem title='Hey' date='this is the date'/>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default NewsletterArchive;
--- src/components/newsletter/newsletterGrid.js
+++ src/components/newsletter/newsletterGrid.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import NewsletterBox from './newsletterBox';
+import NewsletterArchive from './newsletterArchive';
 
 class NewsletterGrid extends Component {
     render() {
         return(
             <div className='newsletter-grid'>
                 <NewsletterBox date={new Date()} />
+                <NewsletterArchive/>
             </div>
         )
     }
