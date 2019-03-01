5e997a2cde5623171f1ddb75f388e2bff68acfac
PC\Adonis <adonis0444@gmail.com>
2019-03-01 16:45:39 -0500

    Adding the submit and cancel button to the new newsletter form

--- src/components/formFields.js
+++ src/components/formFields.js
@@ -12,12 +12,26 @@ export class FormInput extends Component{
     }
 }
 
+export class FormTextArea extends Component{
+    render() {
+        const {className, title, input, type, placeholder} = this.props;
+        return (
+            <div  className={`${className} form-textarea`}>
+                <label className='form-textarea__title'>{title}</label>
+                <textarea className='form-textarea__input' placeholder={placeholder} type={type} {...input}>
+                
+                </textarea>
+            </div>
+        )
+    }
+}
+
 export class FormButton extends Component{
     render() {
-        const {className, title, input, type} = this.props;
+        const {className, title, input, type, small, danger} = this.props;
         return (
-            <div className={`${className} form-button`}>
-                <button className='form-button__button' type={type} {...input}>
+            <div className={`${className} ${small ? 'form-button-small': 'form-button'}`}>
+                <button onClick={() => {this.props.onClick()}} className={`${small ? 'form-button-small': 'form-button'}__button ${danger ? 'form-button-small__danger': ''}`} type={type} {...input}>
                     {title}
                 </button>
             </div>
--- src/components/newsletter/newNewsletterForm.js
+++ src/components/newsletter/newNewsletterForm.js
@@ -2,19 +2,26 @@ import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 
 import FormTitle from '../formTitle';
-import { FormInput, FormButton } from '../formFields';
+import { FormInput, FormButton, FormTextArea } from '../formFields';
 import TextLink from '../textLink';
 
 class NewNewsletterForm extends Component {
+    cancelForm = () => {
+        console.log("This")
+        this.props.history.push('/dashboard');
+    }
+
     render() {
 
         const { handleSubmit } = this.props;
 
         return(
             <form onSubmit={handleSubmit} className='new-newsletter-form'>
-                <FormTitle className='new-newsletter-form__title' text='Login'/>
-                <Field className='new-newsletter-form__email' component={FormInput} name='email' type='email' placeholder='Email' title='Email'/>
-                
+                <FormTitle className='new-newsletter-form__title' text='New Newsletter'/>
+                <Field className='new-newsletter-form__newsletter-title' component={FormInput} name='title' type='text' placeholder='Newsletter Title' title='Newsletter Title'/>
+                <Field className='new-newsletter-form__body' component={FormTextArea} name='body' type='text' placeholder='Newsletter Body' title='Body'/>
+                <Field className='new-newsletter-form__submit' small={true} danger={true} component={FormButton} name='submit' type='submit' title='Submit'/>                
+                <Field className='new-newsletter-form__cancel' onClick={() => { this.cancelForm()}} small={true} component={FormButton} name='cancel' type='button' title='Cancel'/>                
             </form>
         )
     }
