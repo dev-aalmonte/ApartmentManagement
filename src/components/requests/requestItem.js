d112624e5e2e74b9003f9be345fcf1a3979cbc8d
PC\Adonis <adonis0444@gmail.com>
2019-03-04 18:26:09 -0500

    Creating request items component

---
+++ src/components/requests/requestItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class RequestItem extends Component {
+    render() {
+        return(
+            <div className='request-item'>
+                Hello there
+            </div>
+        )
+    }
+}
+
+export default RequestItem;
--- src/components/requests/requests.js
+++ src/components/requests/requests.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import RequestItem from './requestItem';
 
 class Requests extends Component {
     render() {
         return(
             <div className='requests'>
-                
+                <RequestItem/>
             </div>
         )
     }
