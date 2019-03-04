a97d1c45bf788590ced88d9179c142d3a12d87af
PC\Adonis <adonis0444@gmail.com>
2019-03-04 10:14:22 -0500

    Creating the active/inactive style for the request box

--- src/components/requests/requestBoxItem.js
+++ src/components/requests/requestBoxItem.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
  class RequestBoxItem extends Component {
+    handleClick = () => {
+        console.log("Trying to select this box");
+    }
+
     render(){
         const { count, title } = this.props;
         return (
-            <div className='request-box-item'>
+            <div onClick={() => this.handleClick()} className='request-box-item request-box-item-inactive'>
                 <div className='request-box-item__count'>{count}</div>
                 <div className='request-box-item__title'>{title}</div>
                 <div className='request-box-item__point'></div>
