fb08aae57a22f12a429b04d1c844aaae265d5fac
PC\Adonis <adonis0444@gmail.com>
2019-03-06 09:06:42 -0500

    Displaying the correct amount of items in each requestBox

--- src/components/requests/requestBox.js
+++ src/components/requests/requestBox.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import RequestBoxItem from './requestBoxItem';
 
@@ -6,12 +7,47 @@ class RequestBox extends Component {
     render() {
         return(
             <div className='request-box'>
-                <RequestBoxItem count={3} title='Pending'/>
-                <RequestBoxItem count={1} title='In-Progress'/>
-                <RequestBoxItem count={0} title='Complete'/>
+                <RequestBoxItem count={this.props.pendingCount} title='Pending'/>
+                <RequestBoxItem count={this.props.progressCount} title='In-Progress'/>
+                <RequestBoxItem count={this.props.completeCount} title='Complete'/>
             </div>
         )
     }
 }
+
+function mapStateToProps(state) {
+    const { requests } = state.request;
+
+    var pendingCount = 0;
+    var progressCount = 0;
+    var completeCount = 0;
+
+    requests.map(request => {
+        switch (request.status) {
+            case 'Pending':
+            case 'pending':
+                pendingCount++;
+                break;
+
+            case 'In-Progress':
+            case 'in-progress':
+                progressCount++;
+                break;
+
+            case 'Complete':
+            case 'complete':
+                completeCount++;
+                break;
+        }
+    })
+
+    return {
+        pendingCount,
+        progressCount,
+        completeCount
+    }
+}
+
+RequestBox = connect(mapStateToProps)(RequestBox);
 
 export default RequestBox;
